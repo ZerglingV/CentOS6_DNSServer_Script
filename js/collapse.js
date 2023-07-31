399cf0aa3cd7161d39c81cddef52a698111a9fca
ZerglingV <954104749@qq.com>
2023-08-01 00:03:34 +0900

    Site updated: 2023-08-01 00:03:32

--- js/collapse.js
+++ js/collapse.js
@@ -7,16 +7,19 @@ var collapseToggle = function collapseToggle(dom) {
   if (cCont.classList.contains("expanded")) {
     cCont.style.height = cCont.getBoundingClientRect().height + "px";
     cCont.offsetHeight;
+    cCtrl.classList.add("non-radius-border");
     cCont.classList.add("collapsing");
     cCont.classList.remove("collapse-content", "expanded");
     setTimeout(function () {
+      cCtrl.classList.remove("non-radius-border");
       cCont.classList.remove("collapsing");
       cCont.classList.add("collapse-content");
     }, 350);
     cCont.style.height = "";
   } else {
-    cCont.classList.remove("collapse-content");
+    cCtrl.classList.add("non-radius-border");
     cCont.classList.add("collapsing");
+    cCont.classList.remove("collapse-content");
     cCont.style.height = cCont.scrollHeight + "px";
     setTimeout(function () {
       cCont.classList.remove("collapsing");
